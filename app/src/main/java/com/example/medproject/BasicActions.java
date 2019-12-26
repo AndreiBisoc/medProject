4a7116c9d88228759c3428e388b88de93460d08a
m1litaru <militaru.adrian25@gmail.com>
2019-12-26 02:00:16 +0200

    - cand inreegistrezi un pacient/doctor nou, sa aiba si campul id in baza de date ca sa ne fie mai usor ulterior - tastatura sa dispara cand dai click in afara oricarui inputfield

---
+++ app/src/main/java/com/example/medproject/BasicActions.java
@@ -0,0 +1,33 @@
+package com.example.medproject;
+
+import android.app.Activity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BasicActions {
+
+    public static void hideKeyboard(AppCompatActivity activity){
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        //Find the currently focused view, so we can grab the correct window token from it.
+        View view = activity.getCurrentFocus();
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = new View(activity);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+    public static void hideKeyboardWithClick(View element, final AppCompatActivity activity) {
+        element.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                hideKeyboard(activity);
+                return true;
+            }
+        });
+    }
+
+}
